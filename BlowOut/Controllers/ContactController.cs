d8a0c37eca584516e03c621be0366757838b8479
johnnyataisg <42900658+johnnyataisg@users.noreply.github.com>
2018-11-05 17:37:45 -0700

    Add project files.

---
+++ BlowOut/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlowOut.Models;
+
+namespace BlowOut.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.Message += "<p>";
+            ViewBag.Message += "Please call Support at <b style=\"text-decoration: underline;\">801-555-1212</b>. Thank you!</p>";
+            return View();
+        }
+
+        public ActionResult Email(string name, string email)
+        {
+            //"Thank you Scott. We will send an email to test@test.com")
+            ViewBag.Message += "<p>";
+            ViewBag.Message += "Thank you " + name + ". We will send an email to " + email + "</p>";
+            return View();
+        }
+    }
+}
---
+++ BlowOut/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlowOut.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
