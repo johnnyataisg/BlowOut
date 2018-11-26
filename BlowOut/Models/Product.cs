5b6dfdf5cfaeaf207b121ad9eb789be96e58da58
johnnyataisg <42900658+johnnyataisg@users.noreply.github.com>
2018-11-26 14:41:35 -0700

    2018

--- BlowOut/Models/Product.cs
+++ BlowOut/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,10 +13,15 @@ namespace BlowOut.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("Instrument ID")]
         public int instrumentID { get; set; }
+        [DisplayName("Instrument Name")]
         public string desc { get; set; }
+        [DisplayName("New/Used")]
         public string type { get; set; }
+        [DisplayName("Price")]
         public int price { get; set; }
+        [DisplayName("Image URL")]
         public string image { get; set; }
     }
 }
