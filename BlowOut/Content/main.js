97f5e2d7d5694f3fc6c7967054abc3c8035bf408
johnnyataisg <42900658+johnnyataisg@users.noreply.github.com>
2018-11-28 15:19:51 -0700

    2018

--- BlowOut/Content/main.js
+++ BlowOut/Content/main.js
@@ -7,6 +7,15 @@
         $(this).toggleClass("oddrow");
         $(this).toggleClass("highlighted");
     });
+
+    $(".table-row:even").hover(function () {
+        $(this).toggleClass("hovered");
+    });
+
+    $(".table-row:odd").hover(function () {
+        $(this).toggleClass("oddrow");
+        $(this).toggleClass("hovered");
+    });
     
     $('#button').on("click", function () {
         var idList = new Array();
@@ -15,7 +24,7 @@
         });
         $.ajax({
             type: "GET",
-            url: "/Admin/SaveList",
+            url: "/Admin/Delete",
             data: { values: idList },
             success: function (data) {
                 alert(data.Result);
@@ -23,6 +32,9 @@
             contentType: "application/json; charset=utf-8",
             dataType: "json",
             traditional: true
+        });
+        $(".highlighted").each(function () {
+            $(this).remove();
         });
     });
 
--- BlowOut/Controllers/AdminController.cs
+++ BlowOut/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using BlowOut.DAL;
@@ -66,9 +68,60 @@ namespace BlowOut.Controllers
             return View(allOrders);
         }
 
-        public ActionResult SaveList(string[] values)
+        public ActionResult Delete(int[] values)
         {
-            return Json(new { Result = string.Format("First item in list: '{0}'", values[0]) });
+            if (values != null)
+            {
+                foreach (int id in values)
+                {
+                    db.Clients.Remove(db.Clients.Find(id));
+                }
+                db.SaveChanges();
+            }
+            return Json(true);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.instrumentID = new SelectList(db.Products, "instrumentID", "desc", client.instrumentID);
+            return View(client);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "clientID,firstName,lastName,address,city,state,zip,email,phone,instrumentID")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(client).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("DisplayAll");
+            }
+            ViewBag.instrumentID = new SelectList(db.Products, "instrumentID", "desc", client.instrumentID);
+            return View(client); ;
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
         }
     }
 }
